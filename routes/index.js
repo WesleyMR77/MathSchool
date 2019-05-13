1d194c8e67e6889661b8473707b88b7a46606597
WesleyMR77 <wesley.rodrigues@estudante.ifms.edu>
2019-05-13 01:08:49 -0300

    interface de login pronta

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'MathSchool' })
 });
 
-router.get('/conteudo', function(req, res, next) {
-  res.render('conteudo',  { title: 'Conteúdo' });
+router.get('/conteudo', function (req, res, next) {
+  res.render('conteudo', { title: 'Conteúdo' });
 
 });
 
+router.get('/login', function (req, resp, next) {
+  resp.render('login', { title: 'Login' });
+})
 
+router.get('/cadastro', function (req, resp, next) {
+  resp.render('cadastro', { title: 'Cadastro' });
+})
 
-
-  router.get('/login', function (req, resp, next) {  
-    resp.render('login');
-  })
+router.get('/guiaEstudo', function (req, resp, next) {
+  resp.render('guiaEstudo', { title: 'Guia de Estudo' });
+})
 
 module.exports = router;
