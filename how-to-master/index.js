3a2a1628eb95bbf70ced30cf76a694ed7ceaa47a
WesleyMR77 <wesley.rodrigues@estudante.ifms.edu>
2019-05-23 18:27:53 -0300

    servidor rodando no index com imagens e o css

---
+++ how-to-master/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const categorias = require('./routes/categorias')
+const publicacoes = require('./routes/publicacoes')
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded())
+
+const port = process.env.PORT || 3000
+
+app.get('/', async (request, response) => { 
+    response.render('index')
+})
+
+app.use('/categorias', categorias)
+app.use('/publicacoes', publicacoes)
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log('error:', err)
+    } else {
+        console.log('Quase Twitter App está rodando na porta:', port)
+    }
+})
+
