2a101fb75332fb8abc415b32691b315454c7b452
WesleyMR77 <wesley.rodrigues@estudante.ifms.edu>
2019-06-10 11:41:17 -0300

    vizualização do conteúdo faltando apenas ajustes

--- MathSchool App/src/controllers/student/content.js
+++ MathSchool App/src/controllers/student/content.js
@@ -4,17 +4,19 @@ const user = require('../user');
 //Prototipo: Content - Student
 const contentsPage = (req, res) => {
     const profile = user.getAuthUser;
-    const contents = await api.list('contents');
+    // const contents = await api.list('contents');
+    const contents = api.list('contents');
     res.render('student/studentContent', { contents, profile });
 };
 
 //Listar conteudo
 const getContent = async (req, res) => {
     const search = req.body.search;
-    const AllContents = await api.list('contents');
-    let contents = []; 
+    // const AllContents = await   api.list('contents');
+    const AllContents = api.list('contents');
+    let contents = [];
     AllContents.forEach(element => {
-        if(element.name == search || element.author == search){
+        if (element.name == search || element.author == search) {
             contents.push(element);
         }
     });
@@ -24,12 +26,13 @@ const getContent = async (req, res) => {
 
 //Prototipo: Content - Student - Content ID
 const viewContent = async (req, res) => {
-    const content = await api.get('contents/' + req.params.id);
+    // const content = await api.get('contents/' + req.params.id);
+    const content = api.get('contents/' + req.params.id);
     const profile = user.getAuthUser;
     res.render('student/studentContentID', { content, profile });
 };
 
-module.exports = { 
+module.exports = {
     contentsPage,
     getContent,
     viewContent
